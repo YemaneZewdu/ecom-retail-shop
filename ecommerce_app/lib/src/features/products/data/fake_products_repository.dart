896d64f05644a8432de3d7899be49d6c71a45e6d
Yemane <152532702+YemaneInfraTech@users.noreply.github.com>
2024-09-12 23:10:39 +0300

    feat: add future and stream methods

--- ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -6,12 +6,31 @@ class FakeProductsRepository {
   FakeProductsRepository._();
   // making it a singelton with the private constructor
   static FakeProductsRepository instance = FakeProductsRepository._();
+  final List<Product> _products = kTestProducts;
 
   List<Product> getProductsList() {
-    return kTestProducts;
+    return _products;
   }
 
   Product? getProduct(String id){
-    return kTestProducts.firstWhere((product) => product.id == id);
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+// this method is suited for REST APIs to get a single response 
+// since it's a Future the method name should start with "fetch"
+  Future<List<Product>> fetchProductsList(){
+    return Future.value(_products);
+  }
+
+// this method is suited for Real Time DBm, websocket, ...
+// since it's a Stream the method name should start with "watch"
+  Stream<List<Product>> watchProductsList() {
+      return Stream.value(_products);
+  }
+
+// the map will be triggered whenever watchProductsList emits a new value
+// this will give a products list to be filtered 
+  Stream<Product?> watchProduct(String id){
+    return watchProductsList().map((products) => products.firstWhere((product) => product.id == id));
   }
 }
