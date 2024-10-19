a35df3cc5c56fd7b252747bf78de1a8e55ad43ed
Yemane <152532702+YemaneInfraTech@users.noreply.github.com>
2024-10-19 18:29:11 +0300

    feat: implemented FakeAuthRepository

--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,34 +1,50 @@
 // ignore: dangling_library_doc_comments
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-/// This class will be used for Auth services. 
+/// This class will be used for Auth services.
 /// It will be used to read data
 
 class FakeAuthRepository {
+  final _authState = InMemoryStore<AppUser?>(null);
 
-  Stream<AppUser?> authStateChanges() => Stream.value(null);
-
-  AppUser? get currentUser => null;
-
+  Stream<AppUser?> authStateChanges() => _authState.stream;
+  AppUser? get currentUser => _authState.value;
+ 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
   }
-
-   Future<void> createUserWithEmailAndPassword(String email, String password) async {
-    
+ 
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
   }
-
-   Future<void> signOut() async {
-
-   }
+ 
+  Future<void> signOut() async {
+    _authState.value = null;
+  }
+ 
+  void _createNewUser(String email) {
+    // note: the uid could be any unique string. Here we simply reverse the email.
+    _authState.value =
+        AppUser(uid: email.split('').reversed.join(), email: email);
+  }
+ 
+ // just creating this method is not enough so we have to use it 
+ //below in the authRepositoryProvider
+  void dispose() => _authState.close();
 }
-
-/// Provider to access the repository
-/// 
-
+ 
+ // Provider to access the repository
 final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
-  return FakeAuthRepository();
+  final auth = FakeAuthRepository();
+  ref.onDispose(() => auth.dispose());
+  return auth;
 });
  
 final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
--- ecommerce_app/lib/src/utils/in_memory_store.dart
+++ ecommerce_app/lib/src/utils/in_memory_store.dart
@@ -1,7 +1,7 @@
 // this class will be used to handle saving the auth changes because 
 //stream controller doesn't have this feature
 // it will give us a stream 
-// it's like a wrapper over BehaviorSubject
+// it's like a wrapper over BehaviorSubject that's reusable
 import 'package:rxdart/subjects.dart';
 /// An in-memory store backed by BehaviorSubject that can be used to
 /// store the data for all the fake repositories in the app.
