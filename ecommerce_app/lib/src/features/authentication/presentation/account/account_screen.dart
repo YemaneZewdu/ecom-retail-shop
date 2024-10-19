ccc169b5bfcaf8210e72648baa787ddec5f40342
Yemane <152532702+YemaneInfraTech@users.noreply.github.com>
2024-10-19 19:53:32 +0300

    feat:use accountScreenController in widget

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter/material.dart';
@@ -15,32 +16,40 @@ class AccountScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(accountScreenControllerProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'.hardcoded),
+        title: state.isLoading
+            ? const CircularProgressIndicator()
+            : Text('Account'.hardcoded),
         actions: [
           ActionTextButton(
             text: 'Logout'.hardcoded,
-            onPressed: () async {
-              // * Get the navigator beforehand to prevent this warning:
-              // * Don't use 'BuildContext's across async gaps.
-              // * More info here: https://youtu.be/bzWaMpD1LHY
-              final goRouter = GoRouter.of(context);
-              final logout = await showAlertDialog(
-                context: context,
-                title: 'Are you sure?'.hardcoded,
-                cancelActionText: 'Cancel'.hardcoded,
-                defaultActionText: 'Logout'.hardcoded,
-              );
-              if (logout == true) {
-              // haven't used ref.watch because it is used to watch a value
-              // and rebuild the widget based on that 
-              // but here we are in a onPressed callback, whose purpose is 
-              // to do sth 
-               await ref.read(authRepositoryProvider).signOut();
-                goRouter.pop();
-              }
-            },
+            onPressed: state.isLoading
+                ? null
+                : () async {
+                    // * Get the navigator beforehand to prevent this warning:
+                    // * Don't use 'BuildContext's across async gaps.
+                    // * More info here: https://youtu.be/bzWaMpD1LHY
+                    final goRouter = GoRouter.of(context);
+                    final logout = await showAlertDialog(
+                      context: context,
+                      title: 'Are you sure?'.hardcoded,
+                      cancelActionText: 'Cancel'.hardcoded,
+                      defaultActionText: 'Logout'.hardcoded,
+                    );
+                    if (logout == true) {
+                      // haven't used ref.watch because it is used to watch a value
+                      // and rebuild the widget based on that
+                      // but here we are in a onPressed callback, whose purpose is
+                      // to do sth
+                      await ref
+                          .read(accountScreenControllerProvider.notifier)
+                          .signOut();
+                      goRouter.pop();
+                    }
+                  },
           ),
         ],
       ),
