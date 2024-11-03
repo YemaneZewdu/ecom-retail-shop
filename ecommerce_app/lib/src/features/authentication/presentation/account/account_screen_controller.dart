9bb3aeadaa2dfe1002923e36edef369565976aa6
Yemane <152532702+YemaneInfraTech@users.noreply.github.com>
2024-11-03 12:29:34 +0300

    feat: Add AsyncValueUI extension

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -5,25 +5,25 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //StateNotifier is an alternative to ValueNotifier in the Flutter SDK
 // ideal for managing immutable state that widgets can listen to
-class AccountScreenController extends StateNotifier<AsyncValue<void>>{
+class AccountScreenController extends StateNotifier<AsyncValue>{
   // StateNotifier always needs an initial value
   // it needs to be set with the constructor using super()
   // data(null) means "not loading"
  AccountScreenController({required this.authRepository})
-      : super(const AsyncValue<void>.data(null));
+      : super(const AsyncValue.data(null));
   final FakeAuthRepository authRepository;
  
   Future<bool> signOut() async {
     // try {
-    //   state = const AsyncValue<void>.loading();
+    //   state = const AsyncValue.loading();
     //   await authRepository.signOut();
-    //   state = const AsyncValue<void>.data(null);
+    //   state = const AsyncValue.data(null);
     //   return true;
     // } catch (e, stackTrace) {
-    //   state = AsyncValue<void>.error(e, stackTrace);
+    //   state = AsyncValue.error(e, stackTrace);
     //   return false;
     // }
-    state = const AsyncValue<void>.loading();
+    state = const AsyncValue.loading();
     state = await AsyncValue.guard(() => authRepository.signOut());
     return state.hasError == false;
   }
@@ -31,7 +31,7 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>>{
 }
 
 final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
+    AccountScreenController, AsyncValue>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(
     authRepository: authRepository,
