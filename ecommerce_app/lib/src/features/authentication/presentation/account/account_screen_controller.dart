2b36cb6222a48c2448a6d146311350c86001799d
Yemane <152532702+YemaneInfraTech@users.noreply.github.com>
2024-11-03 12:00:58 +0300

    ref: used  AsyncValue.guard() for signout

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -14,15 +14,18 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>>{
   final FakeAuthRepository authRepository;
  
   Future<bool> signOut() async {
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e, stackTrace) {
-      state = AsyncValue<void>.error(e, stackTrace);
-      return false;
-    }
+    // try {
+    //   state = const AsyncValue<void>.loading();
+    //   await authRepository.signOut();
+    //   state = const AsyncValue<void>.data(null);
+    //   return true;
+    // } catch (e, stackTrace) {
+    //   state = AsyncValue<void>.error(e, stackTrace);
+    //   return false;
+    // }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 
 }
